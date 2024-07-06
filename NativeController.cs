7c7591494338060288b73cbead0cca2dd59626ae
ocealot <kaminsky.michael@outlook.com>
2024-07-06 00:59:45 -0400

    Revert "make some internal stuff public"
    This reverts commit d46df414d97ec146d1c93383bc3caf3438c18558.

--- NativeController.cs
+++ NativeController.cs
@@ -1,10 +1,10 @@
+﻿using System;
 using Dalamud.IoC;
 using Dalamud.Plugin;
 using Dalamud.Plugin.Services;
 using FFXIVClientStructs.FFXIV.Component.GUI;
 using KamiToolKit.Classes;
 using KamiToolKit.Nodes;
-using System;
 
 namespace KamiToolKit;
 
@@ -14,77 +14,77 @@ namespace KamiToolKit;
 /// Controller for custom native nodes, this class is required to attach custom nodes to native ui, this service will also keep track of the allocated nodes to prevent memory leaks.
 /// </summary>
 public unsafe class NativeController : IDisposable {
-    [PluginService] private IAddonLifecycle AddonLifecycle { get; set; } // Might be used later, haven't decided yet.
-    [PluginService] private IAddonEventManager AddonEventManager { get; set; }
-    [PluginService] public IFramework Framework { get; set; }
+	[PluginService] private IAddonLifecycle AddonLifecycle { get; set; } // Might be used later, haven't decided yet.
+	[PluginService] private IAddonEventManager AddonEventManager { get; set; }
+	[PluginService] private IFramework Framework { get; set; }
+	
+	public NativeController(IDalamudPluginInterface pluginInterface) {
+		pluginInterface.Inject(this);
+	}
 
-    public NativeController( IDalamudPluginInterface pluginInterface ) {
-        pluginInterface.Inject( this );
-    }
+	/// <summary>
+	/// Dispose this <em>after</em> removing/disposing any attached nodes.
+	/// </summary>
+	public void Dispose() {
+		NodeBase.DisposeAllNodes();
+	}
 
-    /// <summary>
-    /// Dispose this <em>after</em> removing/disposing any attached nodes.
-    /// </summary>
-    public void Dispose() {
-        NodeBase.DisposeAllNodes();
-    }
+	public void AttachToAddon(NodeBase customNode, AtkUnitBase* addon, AtkResNode* target, NodePosition position) {
+		Framework.RunOnFrameworkThread(() => {
+			NodeLinker.AttachNode(customNode.InternalResNode, target, position);
+			customNode.EnableEvents(AddonEventManager, addon);
+			addon->UldManager.UpdateDrawNodeList();
+			addon->UpdateCollisionNodeList(false);
+		});
+	}
 
-    public void AttachToAddon( NodeBase customNode, AtkUnitBase* addon, AtkResNode* target, NodePosition position ) {
-        Framework.RunOnFrameworkThread( () => {
-            NodeLinker.AttachNode( customNode.InternalResNode, target, position );
-            customNode.EnableEvents( AddonEventManager, addon );
-            addon->UldManager.UpdateDrawNodeList();
-            addon->UpdateCollisionNodeList( false );
-        } );
-    }
+	/// <summary>
+	/// Warning! Known to be volatile, use at your own risk.
+	/// </summary>
+	public void AttachToComponent(NodeBase customNode, AtkUnitBase* addon, AtkComponentBase* component, AtkResNode* target, NodePosition position) {
+		Framework.RunOnFrameworkThread(() => {
+			NodeLinker.AttachNode(customNode.InternalResNode, target, position);
+			customNode.EnableEvents(AddonEventManager, addon);
+			component->UldManager.UpdateDrawNodeList();
+		});
+	}
+	
+	public void AttachToNode(NodeBase customNode, NodeBase other, NodePosition position) {
+		Framework.RunOnFrameworkThread(() => {
+			customNode.AttachNode(other, position);
+		});
+	}
 
-    /// <summary>
-    /// Warning! Known to be volatile, use at your own risk.
-    /// </summary>
-    public void AttachToComponent( NodeBase customNode, AtkUnitBase* addon, AtkComponentBase* component, AtkResNode* target, NodePosition position ) {
-        Framework.RunOnFrameworkThread( () => {
-            NodeLinker.AttachNode( customNode.InternalResNode, target, position );
-            customNode.EnableEvents( AddonEventManager, addon );
-            component->UldManager.UpdateDrawNodeList();
-        } );
-    }
+	public void DetachFromAddon(NodeBase customNode, AtkUnitBase* addon) {
+		Framework.RunOnFrameworkThread(() => {
+			customNode.DisableEvents(AddonEventManager);
+			customNode.DetachNode();
+			
+			addon->UldManager.UpdateDrawNodeList();
+			addon->UpdateCollisionNodeList(false);
+		});
+	}
 
-    public void AttachToNode( NodeBase customNode, NodeBase other, NodePosition position ) {
-        Framework.RunOnFrameworkThread( () => {
-            customNode.AttachNode( other, position );
-        } );
-    }
+	/// <summary>
+	/// Warning! Known to be volatile, use at your own risk.
+	/// </summary>
+	public void DetachFromComponent(NodeBase customNode, AtkUnitBase* addon, AtkComponentBase* component) {
+		Framework.RunOnFrameworkThread(() => {
+			customNode.DisableEvents(AddonEventManager);
+			customNode.DetachNode();
 
-    public void DetachFromAddon( NodeBase customNode, AtkUnitBase* addon ) {
-        Framework.RunOnFrameworkThread( () => {
-            customNode.DisableEvents( AddonEventManager );
-            customNode.DetachNode();
+			component->UldManager.UpdateDrawNodeList();
+		});
+	}
 
-            addon->UldManager.UpdateDrawNodeList();
-            addon->UpdateCollisionNodeList( false );
-        } );
-    }
+	public void DetachFromNode(NodeBase customNode) {
+		Framework.RunOnFrameworkThread(customNode.DetachNode);
+	}
 
-    /// <summary>
-    /// Warning! Known to be volatile, use at your own risk.
-    /// </summary>
-    public void DetachFromComponent( NodeBase customNode, AtkUnitBase* addon, AtkComponentBase* component ) {
-        Framework.RunOnFrameworkThread( () => {
-            customNode.DisableEvents( AddonEventManager );
-            customNode.DetachNode();
-
-            component->UldManager.UpdateDrawNodeList();
-        } );
-    }
-
-    public void DetachFromNode( NodeBase customNode ) {
-        Framework.RunOnFrameworkThread( customNode.DetachNode );
-    }
-
-    public void UpdateEvents( NodeBase node, AtkUnitBase* addon ) {
-        Framework.RunOnFrameworkThread( () => {
-            node.EnableEvents( AddonEventManager, addon );
-            addon->UpdateCollisionNodeList( false );
-        } );
-    }
+	public void UpdateEvents(NodeBase node, AtkUnitBase* addon) {
+		Framework.RunOnFrameworkThread(() => {
+			node.EnableEvents(AddonEventManager, addon);
+			addon->UpdateCollisionNodeList(false);
+		});
+	}
 }
